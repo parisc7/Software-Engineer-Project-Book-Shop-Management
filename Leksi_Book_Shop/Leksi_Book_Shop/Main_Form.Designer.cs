80301d568ffd2ea863ddb4fe19c40802793e8a61
Giorgos91 <giorgos18hadjimichael@gmail.com>
2022-04-07 16:01:45 +0300

    16.01

---
+++ Leksi_Book_Shop/Leksi_Book_Shop/Main_Form.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Leksi_Book_Shop
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.logOutButton = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.bookButton = new System.Windows.Forms.Button();
+            this.employeeButton = new System.Windows.Forms.Button();
+            this.customerButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Location = new System.Drawing.Point(264, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(900, 648);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.panel1.Controls.Add(this.logOutButton);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.bookButton);
+            this.panel1.Controls.Add(this.employeeButton);
+            this.panel1.Controls.Add(this.customerButton);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 651);
+            this.panel1.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(18, 479);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(218, 61);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // logOutButton
+            // 
+            this.logOutButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.logOutButton.FlatAppearance.BorderSize = 0;
+            this.logOutButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logOutButton.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logOutButton.Image = global::Leksi_Book_Shop.Properties.Resources.logout;
+            this.logOutButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.logOutButton.Location = new System.Drawing.Point(18, 565);
+            this.logOutButton.Name = "logOutButton";
+            this.logOutButton.Size = new System.Drawing.Size(218, 61);
+            this.logOutButton.TabIndex = 12;
+            this.logOutButton.Text = "Log Out";
+            this.logOutButton.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Image = global::Leksi_Book_Shop.Properties.Resources.email;
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.Location = new System.Drawing.Point(18, 398);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(218, 61);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "   Message";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // bookButton
+            // 
+            this.bookButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.bookButton.FlatAppearance.BorderSize = 0;
+            this.bookButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bookButton.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bookButton.Image = global::Leksi_Book_Shop.Properties.Resources.books;
+            this.bookButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bookButton.Location = new System.Drawing.Point(18, 321);
+            this.bookButton.Name = "bookButton";
+            this.bookButton.Size = new System.Drawing.Size(218, 61);
+            this.bookButton.TabIndex = 10;
+            this.bookButton.Text = "      Books";
+            this.bookButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bookButton.UseVisualStyleBackColor = false;
+            // 
+            // employeeButton
+            // 
+            this.employeeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.employeeButton.FlatAppearance.BorderSize = 0;
+            this.employeeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.employeeButton.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeButton.Image = global::Leksi_Book_Shop.Properties.Resources.Employees;
+            this.employeeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.employeeButton.Location = new System.Drawing.Point(18, 236);
+            this.employeeButton.Name = "employeeButton";
+            this.employeeButton.Size = new System.Drawing.Size(218, 61);
+            this.employeeButton.TabIndex = 9;
+            this.employeeButton.Text = "Employees";
+            this.employeeButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.employeeButton.UseVisualStyleBackColor = false;
+            // 
+            // customerButton
+            // 
+            this.customerButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(113)))), ((int)(((byte)(108)))));
+            this.customerButton.FlatAppearance.BorderSize = 0;
+            this.customerButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customerButton.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerButton.Image = ((System.Drawing.Image)(resources.GetObject("customerButton.Image")));
+            this.customerButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.customerButton.Location = new System.Drawing.Point(18, 157);
+            this.customerButton.Name = "customerButton";
+            this.customerButton.Size = new System.Drawing.Size(218, 61);
+            this.customerButton.TabIndex = 8;
+            this.customerButton.Text = "   Customer";
+            this.customerButton.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(18, 25);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(218, 126);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(29)))), ((int)(((byte)(39)))));
+            this.ClientSize = new System.Drawing.Size(1176, 675);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main_Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button bookButton;
+        private System.Windows.Forms.Button employeeButton;
+        private System.Windows.Forms.Button customerButton;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button logOutButton;
+    }
+}
