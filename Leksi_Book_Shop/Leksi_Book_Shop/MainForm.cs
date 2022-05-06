5b8e925e16aa925c6aa0c4df4e4e05fc18e26199
Giorgos91 <giorgos18hadjimichael@gmail.com>
2022-05-07 02:34:13 +0300

    TIMETABLE CONNECTION

--- Leksi_Book_Shop/Leksi_Book_Shop/MainForm.cs
+++ Leksi_Book_Shop/Leksi_Book_Shop/MainForm.cs
@@ -8,20 +8,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using USB_Barcode_Scanner;
+using System.Data.OleDb;
 
 
 namespace Leksi_Book_Shop
 {
     public partial class MainForm : Form
     {
-        
+        OleDbConnection conn = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C: \Users\Paris Costa\Documents\GitHub\sussy - Softsing - 69 - BAKA\Leksi_Book_Shop\Leksi_Book_Shop\Lexi_Bookshop.accdb");
+        OleDbCommand command = new OleDbCommand();
         BookForm books= new BookForm();
         CustomerForm customers= new CustomerForm();  
         EmployeesForm employees= new EmployeesForm();
         Employee curent=new Employee();
+        DateTime Login;
+        DateTime Logout;
+        
+
        
         public MainForm(bool admin,Employee employee)
         {
+            Login= DateTime.Now;
             customers.Show();
             customers.Hide();
             books.Show();
@@ -37,22 +44,7 @@ namespace Leksi_Book_Shop
                 employeesButton.Visible = false;
             }
         }
-
-        private void Scanner_BarcodeScanned(object sender, BarcodeScannerEventArgs e)
-        {
-            barcodeTxtBox.Text=e.Barcode;
-            foreach (var book in books.BooksList)
-            {
-                if (book.ISBN == int.Parse(barcodeTxtBox.Text))
-                {
-                    
-                    break;
-                }
-               
-            }
-
-        }
-
+       
         private void employeesButton_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -64,21 +56,18 @@ namespace Leksi_Book_Shop
             Company company = new Company();
             company.ShowDialog();
         }
-
         private void customerButton_Click(object sender, EventArgs e)
         {
             this.Hide();
             customers.ShowDialog();
             this.Show();
         }
-
         private void booksButton_Click(object sender, EventArgs e)
         {
             this.Hide();
             books.ShowDialog();
             this.Show();    
         }
-
         private void mailButton_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -86,19 +75,33 @@ namespace Leksi_Book_Shop
             mailForm.ShowDialog();
             this.Show();
         }
-
         private void logoutButton_Click(object sender, EventArgs e)
         {
+            Times();
             this.Close();
         }
-
         private void MainForm_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'lexi_BookshopDataSet.ORDER' table. You can move, or remove it, as needed.
             this.oRDERTableAdapter.Fill(this.lexi_BookshopDataSet.ORDER);
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C: \Users\Paris Costa\Documents\GitHub\sussy - Softsing - 69 - BAKA\Leksi_Book_Shop\Leksi_Book_Shop\Lexi_Bookshop.accdb";
+            command.Connection = conn;
+            command.CommandText = "SELECT * FROM ORDER ";
+        }
+        private void Scanner_BarcodeScanned(object sender, BarcodeScannerEventArgs e)
+        {
+            barcodeTxtBox.Text = e.Barcode;
+            foreach (var book in books.BooksList)
+            {
+                if (book.ISBN == int.Parse(barcodeTxtBox.Text))
+                {
+
+                    break;
+                }
+
+            }
 
         }
-
         private void dailyTotalButton_Click(object sender, EventArgs e)
         {
 
@@ -113,7 +116,15 @@ namespace Leksi_Book_Shop
         {
 
         }
-
+        
+        private void Times()
+        {
+            Logout= DateTime.Now;
+            conn.Open();
+            OleDbCommand cmd = new OleDbCommand("INSERT INTO TIMETABLE (LOG_IN,LOG_OUT,EMPLOYEE_ID) VALUES ('" + Login.TimeOfDay+ "','" +Logout.TimeOfDay + "'," + curent.Employee_id+")", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
         private void searchCustomerButton_Click(object sender, EventArgs e)
         {
             foreach (var customer in customers.CustomerList)
